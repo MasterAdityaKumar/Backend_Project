0558cdb5c345b689adcae0a9bf30e720301129d3
Aditya <aditya.v.impmail@gmail.com>
2024-06-14 08:29:41 +0530

    connected database and util code

---
+++ src/app.js
@@ -0,0 +1,23 @@
+import express from "express"
+import cors from "cors"
+import cookieParser from "cookie-parser"
+
+const app = express()
+
+app.use(cors({
+    origin: process.env.CORS_ORIGIN,
+    credentials:true
+}))
+
+app.use(express.json({limit: "16kb"})) //form bhara tab jo data liya , wo data json format me hota hai
+
+app.use(express.urlencoded({
+    exptended:true,
+    limit:"16kb"
+
+}))
+
+app.use(express.static("public"))
+
+app.use(cookieParser())
+export {app}
